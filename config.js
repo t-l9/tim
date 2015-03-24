6bac367495ff4b3e7def7e910602d5d5f8c78e42
Tim Lauter <timothy.lauter@gmail.com>
2015-03-24 17:51:45 -0400

    changed file structure

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'port' : process.env.PORT || 8000
+    'port' : process.env.PORT || 8000,
+    'database' : 'mongodb://tim:moestavern@proximus.modulusmongo.net:27017/qIjiz7aq'
 };
