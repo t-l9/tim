2c5580c31829ce00ea4bc6dfeb433d978568b4db
Tim Lauter <timothy.lauter@gmail.com>
2015-04-14 18:04:22 -0400

    added nl2br function for post content: needs textarea input

--- public/app/controllers/postCtrl.js
+++ public/app/controllers/postCtrl.js
@@ -2,12 +2,31 @@ angular.module('postCtrl', ['postService'])
     .controller('postController', function(Post, $routeParams) {
         var vm = this;
 
+        // function nl2br(postContent) {
+        //     console.log(postContent);
+        //     return postContent.replace('\n', '<br>');
+        // };
+
+        function nl2br(str) {
+
+            return str.replace(/\\n/g, "<br />");
+        }
+
+
         Post.all()
             .success(function(data) {
                 vm.posts = data;
-                //nl2br function here!!!
+
+                for(i=0; i < vm.posts.length; i++) {
+
+                    data[i].content = nl2br(data[i].content);
+                };
+
+                vm.posts = data;
+
                 console.log(vm.posts);
-            });
+
+             });
 
         Post.get($routeParams.user_id)
             .success(function(data) {
---
+++ public/assets/js/main.js
@@ -0,0 +1,3 @@
+(function() {
+    // private scripts
+})();
