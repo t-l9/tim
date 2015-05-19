26e27990f2b870e610ad772f7809ffca4fadd135
Tim Lauter <timothy.lauter@gmail.com>
2015-05-19 12:46:23 -0400

    name font size/color / article margins / authService returning objects

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -1,87 +1,77 @@
 angular.module('authService', [])
     .factory('Auth', function($http, $q, AuthToken) {
 
-        var authFactory = {};
+        return {
+            login: function(email, password) {
+                return $http.post('/api/authenticate', {
 
-        authFactory.login = function(email, password) {
-            return $http.post('/api/authenticate', {
+                    email: email,
+                    password: password
 
-                email: email,
-                password: password
+                }).success(function(data) {
 
-            }).success(function(data) {
+                    AuthToken.setToken(data.token);
+                    return data;
 
-                AuthToken.setToken(data.token);
-                return data;
+                });
+            },
 
-            });
+            isLoggedIn: function() {
+                if (AuthToken.getToken()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         };
 
-        authFactory.isLoggedIn = function() {
-            if (AuthToken.getToken()) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        return authFactory;
     })
 
     .factory('AuthToken', function($window) {
+        return {
 
-        var authTokenFactory = {};
+            getToken: function() {
+                return $window.localStorage.getItem('token');
+            },
 
-        // get the token out of local storage
-        authTokenFactory.getToken = function() {
-            return $window.localStorage.getItem('token');
+            setToken: function(token) {
+                if (token)
+                    $window.localStorage.setItem('token', token);
+                else
+                    $window.localStorage.removeItem('token');
+            }
         };
-
-        // function to set token or clear token
-        // if a token is passed, set the token
-        // if there is no token, clear it from local storage
-        authTokenFactory.setToken = function(token) {
-            if (token)
-                $window.localStorage.setItem('token', token);
-            else
-                $window.localStorage.removeItem('token');
-        };
-
-	return authTokenFactory;
-
-
     })
 
     .factory('AuthInterceptor', function($q, $location, AuthToken) {
 
-    	var interceptorFactory = {};
+        return {
+            // this will happen on all HTTP requests
+        	request: function(config) {
 
-    	// this will happen on all HTTP requests
-    	interceptorFactory.request = function(config) {
+        		// grab the token
+        		var token = AuthToken.getToken();
 
-    		// grab the token
-    		var token = AuthToken.getToken();
+        		// if the token exists, add it to the header as x-access-token
+        		if (token)
+        			config.headers['x-access-token'] = token;
 
-    		// if the token exists, add it to the header as x-access-token
-    		if (token)
-    			config.headers['x-access-token'] = token;
+        		return config;
+        	},
 
-    		return config;
-    	};
+        	// happens on response errors
+        	responseError: function(response) {
 
-    	// happens on response errors
-    	interceptorFactory.responseError = function(response) {
+        		// if our server returns a 403 forbidden response
+        		if (response.status == 403) {
+        			AuthToken.setToken();
+        			$location.path('/login');
+        		}
 
-    		// if our server returns a 403 forbidden response
-    		if (response.status == 403) {
-    			AuthToken.setToken();
-    			$location.path('/login');
-    		}
+        		// return the errors from the server as a promise
+        		return $q.reject(response);
+        	}
 
-    		// return the errors from the server as a promise
-    		return $q.reject(response);
-    	};
+        };
 
-	    return interceptorFactory;
-
-});
+    });
